3fc48d7b096de8a0960218104ebed88f34078ba7
t123yh <t123yh@outlook.com>
2016-04-22 17:13:28 +0800

    添加用于接收 WndProc 消息的隐藏窗口。

--- PiGeChang/Program.cs
+++ PiGeChang/Program.cs
@@ -8,6 +8,7 @@ namespace PiGeChang
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
 
---
+++ PiGeChang/WndProcReceiverForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiGeChang
+{
+    class WndProcReceiverForm : Form
+    {
+        public WndProcReceiverForm()
+        {
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.ShowInTaskbar = false;
+            this.ShowIcon = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Load += (sender, e) => this.Size = new System.Drawing.Size(1, 1);
+            this.Activated += (sender, e) => this.Hide();
+        }
+
+        public event Action<Message> WndProcReceiving;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (WndProcReceiving != null)
+            {
+                WndProcReceiving(m);
+            }
+        }
+    }
+}
